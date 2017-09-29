2c51a576e5717cd079d9568482c4abae63feb678
Enrique Pérez Arnaud <enrique@cazalla.net>
2017-09-29 14:45:20 +0200

    missing import

--- src/eduid_userdb/__init__.py
+++ src/eduid_userdb/__init__.py
@@ -23,5 +23,6 @@ from eduid_userdb.mail import MailAddress
 from eduid_userdb.phone import PhoneNumber
 from eduid_userdb.nin import Nin
 from eduid_userdb.event import EventList
+from eduid_userdb.password import Password
 from eduid_userdb.tou import ToUEvent
 from eduid_userdb.locked_identity import LockedIdentityNin
